8740a7df0bf00167bbe5789131e891eb49d5f0ca
MattRCole <matthewrichardcole@gmail.com>
2023-04-23 17:47:29 -0600

    Maybe this will work

--- plugin.video.youtube/resources/lib/main.py
+++ plugin.video.youtube/resources/lib/main.py
@@ -7,204 +7,26 @@ Example video plugin that is compatible with Kodi 19.x "Matrix" and above
 """
 import functools
 from typing import Callable, Optional, TypedDict, cast
+
+import m3u8
 from ttml2ssa import Ttml2Ssa
 import requests
 import sys
 from urllib.parse import urlencode, parse_qsl
-import xbmc
+# import xbmc
 import xbmcgui
 import xbmcplugin
 
 from piped.types import Stream, StreamResponse, StreamSubtitle
 
-# ttml2ssa = cast(ttml2ssa.Ttml2Ssa, ttml2ssa)
 
-# Get the plugin url in plugin:// notation.
-_URL = sys.argv[0]
-# Get the plugin handle as an integer number.
+# _URL = sys.argv[0]
 _HANDLE = int(sys.argv[1])
 
-# Free sample videos are provided by www.vidsplay.com
-# Here we use a fixed set of properties simply for demonstrating purposes
-# In a "real life" plugin you will need to get info and links to video files/streams
-# from some web-site or online service.
-VIDEOS = {'Animals': [{'name': 'Crab',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Alligator',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Turtle',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle.mp4',
-                       'genre': 'Animals'}
-                      ],
-            'Cars': [{'name': 'Postal Truck',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic Arrows',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows.mp4',
-                      'genre': 'Cars'}
-                     ],
-            'Food': [{'name': 'Chicken',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbq_chicken-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbqchicken.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Hamburger',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Pizza',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza.mp4',
-                      'genre': 'Food'}
-                     ]}
+def get_subtitle_from_piped(subtitle: Optional[StreamSubtitle], stream: Stream) -> Optional[str]:
+    if subtitle is None:
+        return None
 
-
-def get_url(**kwargs):
-    """
-    Create a URL for calling the plugin recursively from the given set of keyword arguments.
-
-    :param kwargs: "argument=value" pairs
-    :return: plugin call URL
-    :rtype: str
-    """
-    return '{}?{}'.format(_URL, urlencode(kwargs))
-
-
-def get_categories():
-    """
-    Get the list of video categories.
-
-    Here you can insert some parsing code that retrieves
-    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
-    from some site or API.
-
-    .. note:: Consider using `generator functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :return: The list of video categories
-    :rtype: types.GeneratorType
-    """
-    return VIDEOS.keys()
-
-
-def get_videos(category):
-    """
-    Get the list of videofiles/streams.
-
-    Here you can insert some parsing code that retrieves
-    the list of video streams in the given category from some site or API.
-
-    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :param category: Category name
-    :type category: str
-    :return: the list of videos in the category
-    :rtype: list
-    """
-    return VIDEOS[category]
-
-
-def list_categories():
-    """
-    Create the list of video categories in the Kodi interface.
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, 'My Video Collection')
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'videos')
-    # Get video categories
-    categories = get_categories()
-    # Iterate through categories
-    for category in categories:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category)
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-                          'icon': VIDEOS[category][0]['thumb'],
-                          'fanart': VIDEOS[category][0]['thumb']})
-        # Set additional info for the list item.
-        # Here we use a category name for both properties for for simplicity's sake.
-        # setInfo allows to set various information for an item.
-        # For available properties see the following link:
-        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
-        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': category,
-                                    'genre': category,
-                                    'mediatype': 'video'})
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = get_url(action='listing', category=category)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
-
-
-def list_videos(category):
-    """
-    Create the list of playable videos in the Kodi interface.
-
-    :param category: Category name
-    :type category: str
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, category)
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'videos')
-    # Get the list of videos in the category.
-    videos = get_videos(category)
-    # Iterate through videos.
-    for video in videos:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=video['name'])
-        # Set additional info for the list item.
-        # 'mediatype' is needed for skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': video['name'],
-                                    'genre': video['genre'],
-                                    'mediatype': 'video'})
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
-        # Set 'IsPlayable' property to 'true'.
-        # This is mandatory for playable items!
-        list_item.setProperty('IsPlayable', 'true')
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
-        url = get_url(action='play', video=video['video'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = False
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
-
-
-def get_subtitle_from_piped(subtitle: StreamSubtitle, stream: Stream) -> Optional[str]:
     response = requests.get(subtitle['url'], headers={'Accept': 'text/xml;*/*'})
     if response.status_code < 200 or response.status_code >= 300:
         return None
@@ -221,89 +43,102 @@ def get_subtitle_from_piped(subtitle: StreamSubtitle, stream: Stream) -> Optiona
 
 
 def play_video(path):
-    """
-    Play a video by the provided path.
-
-    :param path: Fully-qualified video URL
-    :type path: str
-    """
-    # web_pdb.set_trace()
-
-    # Create a playable item with a path to play.
     response = requests.get(f'https://pipedapi.kavin.rocks/streams/{path}')
-    stream_info: StreamResponse = response.json()
-
-    class Acc(TypedDict):
-        quality: int
-        stream: Stream
-
-    def reducer(
-            acc: Acc,
-            item: Stream,
-            /,
-            get_quality: Callable[[Stream], int],
-            quality_limit: Optional[int] = None,
-        ):
-        # if item["videoOnly"] == True:
-        #     return acc
-        quality = get_quality(item)
-        if quality_limit is not None and quality > quality_limit:
-            return acc
-        if quality >= acc["quality"]:
-            return {
-                "quality": quality,
-                "stream": item
-            }
-        else:
-            return acc
-    initial_acc: Acc = {"quality": 0}
-    stream_choice = functools.reduce(
-        functools.partial(reducer, get_quality=lambda s: int(s['quality'][:-1]), quality_limit=1080),
-        stream_info['videoStreams'],
-        initial_acc,
-    )
-    # audio_choice = functools.reduce(
-    #     functools.partial(reducer, key="bitrate", get_quality=lambda s: s['bitrate']),
-    #     stream_info['audioStreams'],
-    #     initial_acc,
-    # )
-
-    if stream_choice["quality"] == 0:
-        raise ValueError("could not find valid stream")
-
-    stream = stream_choice["stream"]
-    # audio_stream = audio_choice["stream"]
-
-    subtitle = next(
-        (subtitle for subtitle in stream_info['subtitles'] if 'en' in subtitle['code'] and subtitle['autoGenerated'] is False),
-        None
-    )
+    piped_response: StreamResponse = response.json()
     # import web_pdb; web_pdb.set_trace()
 
-    play_item = xbmcgui.ListItem(
-        label=stream_info["title"],
-        label2=stream_info["uploader"],
-        path=stream["url"],
+    playlist_of_playlists = m3u8.load(piped_response['hls'])
+
+    class Acc(TypedDict, total=False):
+        pps: int
+        playlist: m3u8.Playlist
+
+    def playlist_reducer(acc: Acc, playlist: m3u8.Playlist) -> Acc:
+        stream_info = playlist.stream_info
+        if stream_info is None:
+            return acc
+
+        if stream_info.resolution is None or not playlist.uri:
+            return acc
+
+        resolution: tuple[int, int] = stream_info.resolution
+        frame_rate = stream_info.frame_rate if int(stream_info.frame_rate) is not None else 1
+
+        pps = resolution[0] * resolution[1] * frame_rate
+
+        if pps <= acc["pps"]:
+            return acc
+
+        return {"pps": pps, "playlist": playlist}
+
+    [protocol, _, domain, *__] = piped_response['hls'].split('/')
+
+    base_url = f"{protocol}//{domain}"
+
+    playlist_of_playlists
+    playlist_to_grab = functools.reduce(playlist_reducer, list(playlist_of_playlists.playlists), {"pps": 0})["playlist"]
+    main_playlist = m3u8.M3U8(
+        base_path=base_url,
     )
 
-    if subtitle is not None:
-        subtitle_path = get_subtitle_from_piped(subtitle, stream)
-        if subtitle_path is not None:
-            play_item.setSubtitles([subtitle_path])
+    audio_to_grab: str = playlist_to_grab.stream_info.audio
+
+    playlist_to_grab.base_path = f"{base_url}/{playlist_to_grab.base_path}"
+    main_playlist.add_playlist(playlist_to_grab)
+    for m in playlist_of_playlists.media:
+        media = cast(m3u8.Media, m)
+        if media.group_id == audio_to_grab:
+            media.base_path = f"{base_url}/{media.base_path}"
+            main_playlist.add_media(media)
+
+    main_playlist.is_independent_segments = True
+    main_playlist.data["is_independent_segments"] = True
+    playlist_path = "/tmp/piped.m3u8"
+    main_playlist.dump(playlist_path)
 
 
-    play_item.setArt({
-        "thumb": stream_info["thumbnailUrl"]
-    })
-    # videoTag = play_item.getVideoInfoTag()
-    # audio_detail = xbmc.AudioStreamDetail(channels=2, codec=audio_stream["codec"])
-    # audio_detail.
+    # hls_url = f"{protocol}//{domain}/{playlist_to_grab['uri'].removeprefix('/')}"
 
-    # videoTag.addAudioStream()
-    
-    # xbmcplugin.setResolvedUrl(handle=_HANDLE, succeeded=True, listitem=play_item)
 
-    xbmc.Player().play(stream_info["hls"], listitem=play_item)
+    subtitle = next(
+        (subtitle for subtitle in piped_response['subtitles'] if 'en' in subtitle['code'] and subtitle['autoGenerated'] is False),
+        None
+    )
+
+    list_item = xbmcgui.ListItem(
+        label=piped_response["title"],
+        label2=piped_response["uploader"],
+        # path=hls_url,
+        # path=piped_response["hls"],
+        path=playlist_path
+    )
+    list_item.setMimeType('application/x-mpegURL')
+    list_item.setContentLookup(False)
+    list_item.setProperty("inputstream", "inputstream.ffmpegdirect")
+    list_item.setProperty("inputstream.ffmpegdirect.is_realtime_stream", "false")
+    # list_item.setProperty("inputstream.ffmpegdirect.stream_mode", "timeshift")
+    # list_item.setProperty("inputstream.ffmpegdirect.stream_mode", "catchup")
+    list_item.setProperty("inputstream.ffmpegdirect.manifest_type", "hls")
+    list_item.setProperty("inputstream.ffmpegdirect.open_mode", "ffmpeg")
+    # list_item.setProperty("inputstream.ffmpegdirect.playback_as_live", "true")
+    # list_item.setProperty(
+    #     "inputstream.ffmpegdirect.default_programme_duration",
+    #     str(piped_response["duration"]),
+    # )
+
+    # list_item.setProperty('inputstream', 'inputstream.ffmpegdirect')
+    # list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+
+    subtitle_path = get_subtitle_from_piped(subtitle, piped_response['videoStreams'][0])
+    if subtitle_path is not None:
+        list_item.setSubtitles([subtitle_path])
+
+    # list_item.setArt({
+    #     "thumb": piped_response["thumbnailUrl"]
+    # })
+
+    xbmcplugin.setResolvedUrl(handle=_HANDLE, succeeded=True, listitem=list_item)
+
 
 def router(paramstring, action = None):
     """
@@ -313,26 +148,15 @@ def router(paramstring, action = None):
     :param paramstring: URL encoded plugin paramstring
     :type paramstring: str
     """
-    # Parse a URL-encoded paramstring to the dictionary of
-    # {<parameter>: <value>} elements
     params = dict(parse_qsl(paramstring))
-    # Check the parameters passed to the plugin
     if action:
-        if action == 'listing':
-            # Display the list of videos in a provided category.
-            list_videos(params['category'])
-        elif action == 'play':
+        if action == 'play':
             # Play a video from a provided URL.
             play_video(params['video_id'])
         else:
-            # If the provided paramstring does not contain a supported action
-            # we raise an exception. This helps to catch coding errors,
-            # e.g. typos in action names.
             raise ValueError('Invalid paramstring: {}!'.format(paramstring))
-    else:
-        # If the plugin is called from Kodi UI without any parameters,
-        # display the list of video categories
-        list_categories()
+    # else:
+    #     list_categories()
 
 
 if __name__ == '__main__':
